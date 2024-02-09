f3d9507bd8753ef3e86e8b9a7a5598753a88779c
Oleksii Bohoslavskyi <oleks.bohos@gmail.com>
2024-02-09 23:45:47 +0200

    feat

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -2,9 +2,17 @@ import { Component } from 'react';
 import css from './ContactsForm.module.css';
 
 class ContactsForm extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    const name = e.currentTarget.elements.name.value;
+    const number = e.currentTarget.elements.number.value;
+    this.props.addContact({ name, number });
+    e.currentTarget.reset();
+  };
+
   render() {
     return (
-      <form className={css['contacts-form']}>
+      <form className={css['contacts-form']} onSubmit={this.handleSubmit}>
         <label htmlFor="contact-name">Name</label>
         <input
           type="text"
